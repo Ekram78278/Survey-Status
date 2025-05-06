3da25b4edc0632c9ed572cec89ccbc0f9a33dbda
ekram78278@gmail.com <ekram78278@gmail.com>
2025-05-06 09:15:32 +0600

    Add codes

---
+++ first.js
@@ -0,0 +1,46 @@
+/*
+x = "The answer is " + 42; // "The answer is 42"
+y = 42 + " is the answer"; // "42 is the answer"
+z = "37" + 7; // "377"
+console.log(x);*/
+
+/*
+let score = prompt("Enter your grade of 4rth semester:");
+if (score >= 90) {
+    console.log("Grade:A+");
+} else if (score>=80) {
+    console.log("Grade:B");
+} else if (score>=70) {
+    console.log("Grade:C");
+} else if (score>=60) {
+    console.log("Grade:D");
+} else if (score>=50) {
+    console.log ("Grade:E");
+} else (score>=40) {
+    console.log("Grade:F");
+} */
+
+./*
+    let score = prompt("Enter your grade of 4th semester:");
+if (score >= 90) {
+    console.log("Grade: A+");
+} else if (score >= 80) {
+    console.log("Grade: B");
+} else if (score >= 70) {
+    console.log("Grade: C");
+} else if (score >= 60) {
+    console.log("Grade: D");
+} else if (score >= 50) {
+    console.log("Grade: E");
+} else if (score >= 40) {
+    console.log("Grade: F");
+} else {
+    console.log("Fail"); // Handles scores below 40
+} */
+
+
+    let username= prompt("Enter Username");
+    let password= prompt("Enter Password");
+    if username = ("admin"){
+        
+    }
